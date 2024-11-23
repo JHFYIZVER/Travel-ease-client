68356bd1b84f424da6fb7a097715dc9435d22791
Vadim <saraevv205@gmail.com>
2024-11-23 18:12:32 +0900

    fix next-config

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    SERVER: "http://localhost:5000",
-    API_KEY: "test_oX3YfdAqNk-hICnC1lqU4JRB6njI5pN-ZTYn5VCC2jA",
-  },
-};
-
-export default nextConfig;
